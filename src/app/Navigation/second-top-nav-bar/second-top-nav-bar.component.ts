a18b5d3c327c7c937eb2d208af185d2556e7885e
Jonathan Dofka <jtd00007@mix.wvu.edu>
2023-01-25 16:12:09 -0500

    Completed Navbar
    The navbar is complete for the website now. Does require font changes and small tweaks to make it prettier but otherwise working.

--- src/app/Navigation/second-top-nav-bar/second-top-nav-bar.component.ts
+++ src/app/Navigation/second-top-nav-bar/second-top-nav-bar.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './second-top-nav-bar.component.html',
   styleUrls: ['./second-top-nav-bar.component.css']
 })
+
+
+
 export class SecondTopNavBarComponent {
 
 }
