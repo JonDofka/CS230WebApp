9e18cc8f04950f70ff4298621fc365f2144fe616
Jonathan Dofka <jtd00007@mix.wvu.edu>
2023-04-03 13:49:25 -0400

    Lab 7
    Made Graduated Girls Carousel use backend

---
+++ src/app/graduated-girls-carousel/card/girls.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from "@angular/common/http";
+import { ProductModel } from "./product.model";
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+
+export class GirlsService {
+    private baseURL: string = "https://beautycollege-360c1-default-rtdb.firebaseio.com/";
+    private girlsEndPoint: string = "GraduatedGirls.json";
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getGirls() {
+        return this.http.get<ProductModel[]>(this.baseURL + this.girlsEndPoint)
+    }
+}
+
+
+
+
--- src/app/graduated-girls-carousel/graduated-girls-carousel.component.ts
+++ src/app/graduated-girls-carousel/graduated-girls-carousel.component.ts
@@ -1,22 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductModel } from './card/product.model';
-import { mock_list } from './card/mock_list';
+//import { mock_list } from './card/mock_list';
+import { GirlsService } from './card/girls.service';
 
 @Component({
   selector: 'app-graduated-girls-carousel',
   templateUrl: './graduated-girls-carousel.component.html',
   styleUrls: ['./graduated-girls-carousel.component.css']
+  
 })
 
-export class GraduatedGirlsCarouselComponent{
+export class GraduatedGirlsCarouselComponent implements OnInit{
   title = 'TutorialApp';
   students: ProductModel [] = [];
 
-  constructor(){
-    for (var item of mock_list){
-      console.log(item);
-      this.students.push(item);
-    }
+  constructor(private girlsservice:GirlsService){
+    //for (var item of mock_list){
+      //console.log(item);
+      //this.students.push(item);
+    //}
+  }
+
+  ngOnInit(): void {
+    this.girlsservice.getGirls().subscribe((data: ProductModel []) =>{
+      for (var x of data){
+        console.log(x)
+        this.students.push(x)
+      }
+    })
+    
   }
 
 }
